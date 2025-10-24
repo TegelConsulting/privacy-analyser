55fd47be4a899db933730b421f11d1ddcdd24d90
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2025-10-24 14:37:39 +0200

    Removed connectToDb since it does not exists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default async function Home() {
   // Connect to MongoDB (server-side only)
-  await connectToDatabase();
+  // await connectToDatabase();
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
